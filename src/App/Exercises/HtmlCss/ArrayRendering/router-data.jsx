c3fef7c49ce2eee0ac640c63381cb1c6a3b90469
mbialas <michabialas@gmail.com>
2023-05-11 20:00:04 +0200

    array

---
+++ src/App/Exercises/HtmlCss/ArrayRendering/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { ArrayRendering } from './index';
+
+export const blockRouterMetaData = {
+  path: 'Array-rendering',
+  date: '12-12-2023',
+  linkLabel: 'HTML & CSS - Array Rendering',
+  blockNo: 14,
+  element: <ArrayRendering />,
+  tags: ['array', 'render'],
+};
